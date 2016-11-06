4e61fb2e7333baa6c13ce6b5eb85782446379e9a
emma05 <emma.rus93@yahoo.com>
2016-11-07 00:13:00 +0200

    Commit

---
+++ Curs2/Curs2/Mercedes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curs2
+{
+    public class Mercedes
+    {
+        public string StartEngine()
+        {
+            string engine = "Engine started";
+            return engine;
+            //Console.WriteLine("Engine started!");
+        }
+        public string StartEngine(int milliseconds)
+        {
+            return "The car is starting in " + milliseconds + " milliseconds";
+        }
+        public int ShowRemainingNoOfKM(int noOfFuelLiters, int Consumption)
+        {
+            //Console.WriteLine("Remaining km:" + noOfFuelLiters / Consumption * 100);
+            return noOfFuelLiters / Consumption;
+        }
+        public double CalculateConsumption(params int[] kms)
+        {
+            double sum = 0;
+            foreach (int km in kms)
+                sum += km;
+            return sum / 100 * 7;
+        }
+    }
+}
+
+
---
+++ Curs2/Curs2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curs2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mercedes _mercedes = new Mercedes();
+
+            //_mercedes.StartEngine();
+            Console.WriteLine(_mercedes.StartEngine());
+            // _m.ShowRemainingNoOfKM(200, 10);
+            Console.WriteLine(_mercedes.ShowRemainingNoOfKM(200, 10));
+            Console.WriteLine(_mercedes.StartEngine(30));
+            Console.WriteLine(_mercedes.CalculateConsumption(4, 5, 6));
+            Console.ReadLine();
+
+        }
+    }
+}
