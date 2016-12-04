0ab9536059f74722c384122971ecd85d7529eacc
emma05 <emma.rus93@yahoo.com>
2016-12-04 23:54:54 +0200

    Commit MVC_Course_7

---
+++ MVC_Course_7/MVC_Course_7/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Course_7.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index() {
+            return View("Index");
+        }
+        [ActionName("about")]
+        public ActionResult SomeMethodName(){
+            return View("SomeMethodName");
+        }
+        //[NonAction]
+        public ActionResult Calc() {
+            return Content("Calc");
+        }
+        [HttpGet]
+        public ActionResult VerbsTest()
+        {
+            return Content("Hello");
+        }
+        [ActionName("authorize")]
+        [Authorize(Roles = "Admin")]
+        public ActionResult ThisMethodNeedsAuthorization() {
+            return Content("Hello");
+        }
+    }
+}
